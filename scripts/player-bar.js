ef465e0af4973127bd3e115c7a2f24fe9938d699
Jeremiah Judkins <jeremiahjudkins@Jeremiah-Judkinss-iMac.local>
2018-01-14 14:49:44 -0800

    Checkpoint-20-BlocJams-jQuery-sliders2

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,6 +13,22 @@
     player.playPause(nextSong);
   });
 
+  $('#time-control input').on('input', function(event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval ( () => {
+  //We don't want our setInterval callback function to do anything if a song
+// isn't currently playing, so add a line that executes a return statement
+// If player.playState doesn't equal 'playing'.
+  if (player.playState !== 'playing') { return; }
+  const currentTime = player.getTime();
+  const duration = player.getDuration();
+  const percent = (currentTime/ duration) * 100;
+  $('#time-control .current-time').text( currentTime );
+  $('#time-control input').val(percent);
+  }, 1000);
+
 
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') {return; }
